2463af8779b932d85778f0b0ea5d4452476e66aa
Neil Chazin <nchazin@salesforce.com>
2022-12-03 06:58:03 -0800

    day3.py

---
+++ day3/day3.py
@@ -0,0 +1,42 @@
+import sys
+
+
+with open(sys.argv[1]) as f:
+    data = f.readlines()
+
+
+def make_compartments(items):
+    mid = int(len(items) / 2)
+    compartment1 = set(items[:mid])
+    compartment2 = set(items[mid:])
+
+    return [compartment1, compartment2]
+
+
+def get_priority(item):
+    if item > "Z":
+        priority = ord(item) - ord("a") + 1
+    else:
+        priority = ord(item) - ord("A") + 27
+    return priority
+
+
+value = 0
+for line in data:
+    line = line.strip()
+    c1, c2 = make_compartments(list(line))
+    common = c1.intersection(c2).pop()
+    value += get_priority(common)
+
+print(value)
+
+value = 0
+for b in range(0, len(data), 3):
+    b1 = set(list(data[b].strip()))
+    b2 = set(list(data[b + 1].strip()))
+    b3 = set(list(data[b + 2].strip()))
+
+    badge = b1.intersection(b2).intersection(b3).pop()
+    value += get_priority(badge)
+
+print(value)
