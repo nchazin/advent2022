73bf41bc49dfba261225ec695e1d06706472b4e7
Neil Chazin <nchazin@salesforce.com>
2022-12-04 21:53:38 -0800

    day5 done

--- day5/day5.py
+++ day5/day5.py
@@ -4,47 +4,76 @@ import math
 with open(sys.argv[1]) as f:
     data = f.readlines()
 
-stacksize = 0
-stacks = []
 
-#setup the stacks
-for line in data:
-    line = line.strip('\n')
-    if line.find('[') == -1:
-        stacks = [list(reversed(s)) for s in stacks]
-        break
-    if stacksize== 0:
-        stacksize = math.ceil(len(line)/4.0)
-        for s in range(stacksize):
-            stacks.append(list())
-    for item in range(stacksize):
-        index = item*4
-        crate = line[index+1]
-        if crate != ' ':
-            stacks[item].append(crate)
+def make_stacks():
+    stacksize = 0
+    stacks = []
 
-print(stacks)
+    # setup the stacks
+    for line in data:
+        line = line.strip("\n")
+        if line.find("[") == -1:
+            stacks = [list(reversed(s)) for s in stacks]
+            break
+        if stacksize == 0:
+            stacksize = math.ceil(len(line) / 4.0)
+            for s in range(stacksize):
+                stacks.append(list())
+        for item in range(stacksize):
+            index = item * 4
+            crate = line[index + 1]
+            if crate != " ":
+                stacks[item].append(crate)
+    return stacks
 
-#find the instructiosn and do them    
+
+stacks = make_stacks()
+
+# find the instructiosn and do them
 in_commands = False
 for line in data:
-    line = line.strip('\n')
-    if line == '':
+    line = line.strip("\n")
+    if line == "":
         in_commands = True
         continue
     if in_commands:
-        parts = line.split(' ')
+        parts = line.split(" ")
         count = int(parts[1])
         mfrom = int(parts[3])
         mto = int(parts[5])
-        print(f"Move {count} from {mfrom} to {mto}")
         for i in range(count):
-            c = stacks[mfrom-1].pop()
-            stacks[mto-1].append(c)
+            c = stacks[mfrom - 1].pop()
+            stacks[mto - 1].append(c)
 
-print(stacks)
-for x in stacks:
-    print(x[-1], end="")    
-print()
-    
-      
+
+def print_stacks(stacks):
+    for x in stacks:
+        print(x[-1], end="")
+    print()
+
+
+print_stacks(stacks)
+
+
+stacks = make_stacks()
+
+in_commands = False
+for line in data:
+    line = line.strip("\n")
+    if line == "":
+        in_commands = True
+        continue
+    if in_commands:
+        parts = line.split(" ")
+        count = int(parts[1])
+        mfrom = int(parts[3])
+        mto = int(parts[5])
+        to_add = []
+        for i in range(count):
+            c = stacks[mfrom - 1].pop()
+            to_add.append(c)
+        to_add.reverse()
+        for c in to_add:
+            stacks[mto - 1].append(c)
+
+print_stacks(stacks)
