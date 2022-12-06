448da29e9c9743f7c800a149d91da05e4b5e9792
Neil Chazin <nchazin@salesforce.com>
2022-12-06 15:01:14 -0800

    add a daily basefile

---
+++ base.py
@@ -0,0 +1,17 @@
+import sys
+import aocd
+
+
+if len(sys.argv) > 2 and sys.argv[2] == "submit":
+    SUBMIT = True
+else:
+    SUBMIT = False
+
+
+def submit(val, part, day, year):
+    if SUBMIT:
+        aocd.submit(val, part=part, day=day, year=year)
+
+
+with open(sys.argv[1]) as f:
+    data = f.read()
