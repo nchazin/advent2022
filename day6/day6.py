a056fbf3f74b0e8a80c2ad874e183f4176033ffb
Neil Chazin <nchazin@salesforce.com>
2022-12-05 22:47:59 -0800

    cleanup and testcases

--- day6/day6.py
+++ day6/day6.py
@@ -2,22 +2,31 @@ import sys
 import aocd
 
 
-set4 = set()
-list4 = list()
 with open(sys.argv[1]) as f:
     data = f.read()
 
+
+def finder(data, buflen):
+    for i in range(buflen, len(data)):
+        if len(set(data[i - buflen : i])) == buflen:
+            return i
+
+
+s1 = finder(data, 4)
+print(s1)
+aocd.submit(s1, part="a", day=6, year=2022)
+s2 = finder(data, 14)
+print(s2)
+aocd.submit(s2, part="b", day=6, year=2022)
+
 for i in range(4, len(data)):
-    if data[i] not in data[i-4:i] and len(set(data[i-4:i])) ==4:
+    if len(set(data[i - 4 : i])) == 4:
         print(i)
-        #aocd.submit(i,  part="a", day=6, year=2022)
+        aocd.submit(i, part="a", day=6, year=2022)
         break
 
-print('--------')
-
 for i in range(14, len(data)):
-    if len(set(data[i-14:i])) ==14:
-        print(data[i-14:i])
-        print(set(data[i-14:i+1]))
+    if len(set(data[i - 14 : i])) == 14:
         print(i)
-        aocd.submit(i,  part="b", day=6, year=2022)
+        aocd.submit(i, part="b", day=6, year=2022)
+        break
