1ccea9a038f2bde53405dc2fe50d0ab52d886b15
Neil Chazin <nchazin@salesforce.com>
2022-12-07 07:23:46 -0800

    cleanup

--- day7/day7.py
+++ day7/day7.py
@@ -13,37 +13,36 @@ class filetypes(Enum):
     dir = 0
     file = 1
 
+
 def submit(val, part, day, year):
     if SUBMIT:
         aocd.submit(val, part=part, day=day, year=year)
 
 
-
-
-class File():
+class File:
     def __init__(self, name, type, size=None, parent=None):
-        self.name=name
-        self.type=type
-        self.size=size
-        self.parent=parent
+        self.name = name
+        self.type = type
+        self.size = size
+        self.parent = parent
         self.children = []
 
     def __repr__(self):
-        return repr(str(self.type) + " "+  self.name + " " +  str(self.size))
+        return repr(str(self.type) + " " + self.name + " " + str(self.size))
 
     def get_size(self):
         if self.type == filetypes.file:
-             return self.size
+            return self.size
         else:
-             return self.dir_size()
+            return self.dir_size()
 
     def dir_size(self):
         size = 0
         for c in self.children:
             size += c.get_size()
-        print(f"dir: {self.name} size: {size}")
         return size
-        
+
+
 with open(sys.argv[1]) as f:
     data = f.read()
 
@@ -51,27 +50,23 @@ root = File("/", filetypes.dir)
 curdir = None
 alldirs = [root]
 
+
 def build_tree(data):
-    datalines = data.split('\n')
+    datalines = data.split("\n")
     i = 0
     while i < len(datalines):
         line = datalines[i]
-        parts = line.split(' ')
+        parts = line.split(" ")
         if len(parts) < 2:
             break
-        elif parts[1] == 'cd':
+        elif parts[1] == "cd":
             i += 1
-            print(line)
-            if parts[2] == '/':
+            if parts[2] == "/":
                 curdir = root
-                print(f"cd to {curdir}")
-            elif parts[2] == '..':
+            elif parts[2] == "..":
                 curdir = curdir.parent
-                print(f".. to {curdir}")
             else:
                 found = False
-                print(parts[2])
-                print(curdir)
                 for item in curdir.children:
                     if item.type == filetypes.dir and item.name == parts[2]:
                         curdir = item
@@ -79,43 +74,39 @@ def build_tree(data):
                         break
                 if not found:
                     raise RuntimeError("Could not find " + parts[2])
-        elif parts[1] == 'ls':
-            i+=1
+        elif parts[1] == "ls":
+            i += 1
             while True:
                 line = datalines[i]
-                if line == "" or  line[0] == '$':
+                if line == "" or line[0] == "$":
                     break
                 size, name = line.split(" ")
-                if size == 'dir':
+                if size == "dir":
                     newfile = File(name, filetypes.dir)
                     alldirs.append(newfile)
                 else:
                     newfile = File(name, filetypes.file, int(size))
-                print(f"adding {newfile} to {curdir}")
                 newfile.parent = curdir
                 curdir.children.append(newfile)
                 i += 1
         else:
             raise RuntimeError("Unexpectd line: " + line)
-                
+
 
 build_tree(data)
 
 dir_sizes = [d.dir_size() for d in alldirs]
 smalldirs = [d.dir_size() for d in alldirs if d.dir_size() <= 100000]
 
-print(sum(smalldirs))
 
 submit(sum(smalldirs), "a", 7, 2022)
 
 total_size = root.dir_size()
-free_space = 70000000- total_size
+free_space = 70000000 - total_size
 needed_space = 30000000
 
-big_enough = [d for d in alldirs if  free_space + d.dir_size() >= needed_space]
-big_enough_sizes = [d.dir_size() for d in big_enough]
-big_enough_sizes.sort()
-print(big_enough)
-submit(big_enough_sizes[0], "b", 7, 2022)
-
-
+big_enough = [
+    d.dir_size() for d in alldirs if free_space + d.dir_size() >= needed_space
+]
+big_enough.sort()
+submit(big_enough[0], "b", 7, 2022)
