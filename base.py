b8352e608c0fd5991feb5c598b2784412b47cf59
Neil Chazin <nchazin@salesforce.com>
2022-12-08 11:18:37 -0800

    Add day8 part2

--- base.py
+++ base.py
@@ -11,6 +11,8 @@ else:
 def submit(val, part, day, year):
     if SUBMIT:
         aocd.submit(val, part=part, day=day, year=year)
+    else:
+        print(f"Not submiting {val} for 12/{day}/{year} part {part}")
 
 
 with open(sys.argv[1]) as f:
--- day8/day8.py
+++ day8/day8.py
@@ -11,14 +11,15 @@ else:
 def submit(val, part, day, year):
     if SUBMIT:
         aocd.submit(val, part=part, day=day, year=year)
+    else:
+        print(f"Not submiting {val} for 12/{day}/{year} part {part}")
 
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
 
 forest = list(data)
-forest = [r.replace("\n","") for r in forest]
-#print(forest)
+forest = [r.replace("\n", "") for r in forest]
 
 
 def solve1(forest):
@@ -31,68 +32,59 @@ def solve1(forest):
         for j in range(rowlen):
             k = forest[i][j]
 
-            #look up
+            # look up
             VISUP = True
             UPCOUNT = 0
-            for ih in range(i-1,-1,-1):
+            for ih in range(i - 1, -1, -1):
                 if forest[ih][j] >= k:
-                    UPCOUNT = i-ih
+                    UPCOUNT = i - ih
                     VISUP = False
                     break
-            #if VISUP:
-            #    UPCOUNT = i
-            #print(f"{i}, {j}, {k}, {VISUP}")
+            if VISUP and i > 0:
+                UPCOUNT = i
 
-            #lookdown
+            # lookdown
             VISDOWN = True
             DOWNCOUNT = 0
-            for ih in range(i+1,forelen):
+            for ih in range(i + 1, forelen):
                 if forest[ih][j] >= k:
                     VISDOWN = False
-                    DOWNCOUNT = ih-i
+                    DOWNCOUNT = ih - i
                     break
-            #if VISDOWN:
-            #    DOWNCOUNT = forelen - 1
-            #print(f"{i}, {j}, {k}, {VISDOWN}")
+            if VISDOWN and i < forelen - 1:
+                DOWNCOUNT = forelen - i - 1
 
-            #lookleft
+            # lookleft
             VISLEFT = True
             LEFTCOUNT = 0
-            for jh in range(j-1,-1,-1):
+            for jh in range(j - 1, -1, -1):
                 if forest[i][jh] >= k:
                     VISLEFT = False
-                    LEFTCOUNT = j-jh
+                    LEFTCOUNT = j - jh
                     break
-            #print(f"{i}, {j}, {k}, {VISLEFT}")
-            #if VISLEFT:
-            #    VISCOUNT = j
+            if VISLEFT and j > 0:
+                LEFTCOUNT = j
 
-            #lookright
-            VISRIGHT= True
+            # lookright
+            VISRIGHT = True
             RIGHTCOUNT = 0
-            for jh in range(j+1, rowlen):
+            for jh in range(j + 1, rowlen):
                 if forest[i][jh] >= k:
                     VISRIGHT = False
                     RIGHTCOUNT = jh - j
                     break
-            #print(f"{i}, {j}, {k}, {VISLEFT}")
-            #if VISRIGHT:
-            #    RIGHTCOUNT = rowlen - j
+            if VISRIGHT and j < rowlen - 1:
+                RIGHTCOUNT = rowlen - j - 1
 
             if VISUP or VISDOWN or VISLEFT or VISRIGHT:
                 VISCOUNT += 1
             score = UPCOUNT * DOWNCOUNT * RIGHTCOUNT * LEFTCOUNT
-            #print ("--------------------")
-            #print(f"{UPCOUNT} {DOWNCOUNT} {RIGHTCOUNT} {LEFTCOUNT}")
-            #print(f"{i} {j} {k} {score}")
-            #print ("--------------------")
             if score > MAXSCORE:
                 MAXSCORE = score
 
-    print(MAXSCORE)
-    return(VISCOUNT)
+    return [MAXSCORE, VISCOUNT]
 
 
-result = solve1(forest)
-print(">",  result)
-submit(result, 'a', 8, 2022)
+score, count = solve1(forest)
+submit(count, "a", 8, 2022)
+submit(score, "b", 8, 2022)
