aa0ce90df106b15ff4a6e14bf8d65132ad28416d
Neil Chazin <nchazin@salesforce.com>
2022-12-15 13:00:46 -0800

    perf up

--- day15/day15.py
+++ day15/day15.py
@@ -2,6 +2,7 @@ import sys
 import aocd
 
 from math import inf, floor
+from functools import cmp_to_key
 
 import re
 
@@ -21,9 +22,9 @@ def submit(val, part, day, year):
 
 with open(sys.argv[1]) as f:
     data = f.readlines()
-print(sys.argv)
+
 if len(sys.argv) > 2:
-    checkline = int(sys.argv[2])
+    checkline = 2000000
 else:
     checkline = 10
 
@@ -68,20 +69,79 @@ for line in data:
 print(f"checking line {checkline}")
 
 
-sweeps = set()
-for sensor in sensors:
-    based = sensor.md
-    s = sensor.sensor
-    straightd = abs(s[1] - checkline)
-    w = based * 2 + 1
-    w -= 2 * straightd
-    if w > 0:
-        w = floor(w / 2)
-        for x in range(s[0] - w, s[0] + w + 1):
-            sweeps.add((x, checkline))
+def merge(a, b):
+    return (min(a[0], b[0]), max(a[1], b[1]))
 
 
-for sensor in sensors:
-    if sensor.beacon in sweeps:
-        sweeps.remove(sensor.beacon)
-submit(len(sweeps), "a", 15, 2022)
+def mergeable(a, b):
+    return _mergeable(a, b) or _mergeable(b, a)
+
+
+def _mergeable(a, b):
+    return a[0] >= b[0] and a[0] <= b[1]
+
+
+def compare_sweeps(a, b):
+    if a[0] < b[0]:
+        return -1
+    elif a[0] > b[0]:
+        return 1
+    else:
+        return 0
+
+
+def mergeall(sweeps):
+    old_len = len(sweeps)
+    new_len = 0
+    while new_len != old_len:
+        old_len = len(sweeps)
+        newsweeps = set()
+        sweepsl = sorted(list(sweeps), key=cmp_to_key(compare_sweeps))
+        i = 0
+        while i < len(sweepsl) - 1:
+            a = sweepsl[i]
+            b = sweepsl[i + 1]
+            if mergeable(a, b):
+                newsweep = merge(a, b)
+                newsweeps.add(newsweep)
+                i += 2
+            else:
+                newsweeps.add(a)
+                i += 1
+        if len(newsweeps) > 0:
+            sweeps = newsweeps
+        new_len = len(sweeps)
+    return sweeps
+
+
+def sensor_sweep(checkline):
+    sweeps = set()
+    for sensor in sensors:
+        based = sensor.md
+        s = sensor.sensor
+        straightd = abs(s[1] - checkline)
+        w = based * 2 + 1
+        w -= 2 * straightd
+        if w > 0:
+            w = floor(w / 2)
+            sweep = (s[0] - w, s[0] + w)
+            merged = False
+            for _sweep in sweeps:
+                # check for merges
+                if mergeable(sweep, _sweep):
+                    sweeps.remove(_sweep)
+                    sweeps.add(merge(sweep, _sweep))
+                    merged = True
+                    break
+            if not merged:
+                sweeps.add(sweep)
+
+    return mergeall(sweeps)
+
+
+sweeps = sensor_sweep(checkline)
+
+
+print(sum(x[1] - x[0] for x in sweeps))
+
+submit(sum(x[1] - x[0] for x in sweeps), "a", 15, 2022)
