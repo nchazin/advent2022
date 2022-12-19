634f79c11c7f26d296d64a776c0f78597c178ced
Neil Chazin <nchazin@salesforce.com>
2022-12-18 20:27:56 -0800

    day18 pt 2

--- day18/day18.py
+++ day18/day18.py
@@ -24,15 +24,65 @@ for line in data:
     (x, y, z) = line.strip().split(",")
     cubes.add((int(x), int(y), int(z)))
 
-misses = 0
+misses = set()
+miss_count = 0
 for cube in cubes:
     x, y, z = cube
     for diff in [+1, -1]:
         if (x + diff, y, z) not in cubes:
-            misses += 1
+            misses.add((x + diff, y, z))
+            miss_count += 1
         if (x, y + diff, z) not in cubes:
-            misses += 1
+            misses.add((x, y + diff, z))
+            miss_count += 1
         if (x, y, z + diff) not in cubes:
-            misses += 1
+            misses.add((x, y, z + diff))
+            miss_count += 1
 
-submit(misses, "a", 18, 2022)
+submit(miss_count, "a", 18, 2022)
+
+
+minx = min(c[0] for c in cubes) - 1
+maxx = max(c[0] for c in cubes) + 1
+miny = min(c[0] for c in cubes) - 1
+maxy = max(c[0] for c in cubes) + 1
+minz = min(c[2] for c in cubes) - 1
+maxz = max(c[2] for c in cubes) + 1
+
+start = tuple([minx, miny, minz])
+to_visit = [start]
+outside_seen = set()
+
+
+def neighbors(c):
+    x, y, z = c
+    for dx, dy, dz in [
+        [1, 0, 0],
+        [-1, 0, 0],
+        [0, 1, 0],
+        [0, -1, 0],
+        [0, 0, 1],
+        [0, 0, -1],
+    ]:
+        yield (x + dx, y + dy, z + dz)
+
+
+while len(to_visit) > 0:
+    cur = to_visit.pop()
+
+    outside_seen.add(cur)
+    for n in neighbors(cur):
+        x, y, z = n
+        if x < minx or x > maxx or y < miny or y > maxy or z < miny or z > maxz:
+            continue
+        if n in outside_seen or n in cubes:
+            continue
+        to_visit.append(n)
+
+hits_count = 0
+for cube in outside_seen:
+    for neighbor in neighbors(cube):
+        if neighbor in cubes:
+            hits_count += 1
+
+submit(hits_count, "b", 18, 2022)
