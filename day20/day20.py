86c26ce7022e77e5261d80a735bd49b74eb5e68b
Neil Chazin <nchazin@salesforce.com>
2022-12-20 11:53:28 -0800

    day20 pt2

--- day20/day20.py
+++ day20/day20.py
@@ -19,29 +19,50 @@ def submit(val, part, day, year):
 with open(sys.argv[1]) as f:
     data = f.readlines()
 
-q = deque()
-for i, x in enumerate(data):
-    q.append((i, int(x)))
-print(f"starting: {q}")
-for i in range(len(q)):
-    # find the ith element
-    while q[0][0] != i:
-        q.rotate(-1)
-    print(i)
-    p, n = q.popleft()
-    q.rotate(-1 * n)
-    q.appendleft((p, n))
-    # print(q)
+
+def mix(data, key=1, mixes=1):
+    q = deque()
+    for i, x in enumerate(data):
+        q.append((i, int(x) * key))
+    print(f"starting: {q}")
+    for _ in range(mixes):
+        for i in range(len(q)):
+            # find the ith element (based on origina order)
+            while q[0][0] != i:
+                q.rotate(-1)
+            p, n = q.popleft()
+            q.rotate(-1 * n)
+            q.appendleft((p, n))
+    return q
 
 
-final = deque([x[1] for x in q])
-zero_i = final.index(0)
+def get_answer(q):
+    final = deque([x[1] for x in q])
+    zero_i = final.index(0)
 
-answer = sum((final[(zero_i + off) % len(q)] for off in (1000, 2000, 3000)))
+    answer = sum((final[(zero_i + off) % len(q)] for off in (1000, 2000, 3000)))
+    return answer
+
+
+q = mix(data)
+
+answer = get_answer(q)
+
 print(answer)
 submit(
     answer,
     "a",
+    20,
+    2020,
+)
+
+q = mix(data, key=811589153, mixes=10)
+answer = get_answer(q)
+
+print(answer)
+submit(
+    answer,
+    "b",
     20,
     2020,
 )
